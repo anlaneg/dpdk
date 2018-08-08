5b6833a05b1302a636c391442abdaa43d000ff96
anlaneg <anlaneg@126.com>
2018-08-08 22:20:29 +0800

    update do.sh

--- do.sh
+++ do.sh
@@ -44,7 +44,13 @@ install
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 
 make config T=$RTE_TARGET
+<<<<<<< master
 make -j20
 #make install -j20 T=$RTE_TARGET
 make install -j20 DESTDIR=$RTE_TARGET
+=======
+make V=1 #-j20
+#make install -j20 T=$RTE_TARGET
+make V=1 install -j20 T=$RTE_TARGET DESTDIR=$RTE_TARGET
+>>>>>>> local
 
