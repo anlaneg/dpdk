98d73aec6692b1b3a537ca9c71e57bb30e1b7831
Li Zhang <lizh@nvidia.com>
2021-05-13 11:35:44 +0300

    net/mlx5: fix metering cleanup on stop
    A meter may handle Rx queue reference in his sub-policies.
    In stop operation, all the Rx queues are released.
    
    Wrongly, the meter reference was not released before
    destroying the Rx queues what cause an error in stop.
    
    Release the Rx queues meter references in stop operation.
    
    Fixes: fc6ce56bba05 ("net/mlx5: prepare sub-policy for flow with meter")
    
    Signed-off-by: Li Zhang <lizh@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>

--- drivers/net/mlx5/mlx5.h
+++ drivers/net/mlx5/mlx5.h
@@ -669,6 +669,8 @@ struct mlx5_meter_policy_action_container {
 		/* Index to port ID action resource. */
 		void *dr_jump_action[MLX5_MTR_DOMAIN_MAX];
 		/* Jump/drop action per color. */
+		uint16_t queue;
+		/* Queue action configuration. */
 	};
 };
 
@@ -682,6 +684,8 @@ struct mlx5_flow_meter_policy {
 	/* Rule applies to egress domain. */
 	uint32_t transfer:1;
 	/* Rule applies to transfer domain. */
+	uint32_t is_queue:1;
+	/* Is queue action in policy table. */
 	rte_spinlock_t sl;
 	uint32_t ref_cnt;
 	/* Use count. */
@@ -1656,6 +1660,7 @@ struct mlx5_flow_meter_policy *mlx5_flow_meter_policy_find
 		uint32_t *policy_idx);
 int mlx5_flow_meter_flush(struct rte_eth_dev *dev,
 			  struct rte_mtr_error *error);
+void mlx5_flow_meter_rxq_flush(struct rte_eth_dev *dev);
 
 /* mlx5_os.c */
 struct rte_pci_driver;
